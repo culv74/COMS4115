c76118e07a2a2ff3fea24ced542eb92bb3ad52dc
friedmanholley <gf2501@columbia.edu>
2024-11-12 20:09:45 -0500

    Update parsertestprog.py

--- parsertestprog.py
+++ parsertestprog.py
@@ -5,8 +5,6 @@ from parser import Parser
 # Sample source code to test the lexer and parser
 source_code = """
 draw(sun + dog);
-grid(2, 2, draw(sun), write(dog), write(tree), draw(cat));
-(3 * draw(dog)) / write(I like dogs);
 """
 
 # Step 1: Tokenize the source code using the Lexer
